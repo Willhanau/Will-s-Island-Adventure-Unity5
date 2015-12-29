508bd466149b9d250152d195e2525138abea88c7
willhanau <willhanau@gmail.com>
2015-12-29 13:16:13 -0800

    Chapter 5 Completed!

---
+++ UGDE V2/Assets/Scripts/DoorManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorManager : MonoBehaviour
+{
+
+    private bool doorIsOpen = false;
+    private float doorTimer = 0.0f;
+
+    public float doorOpenTime = 3.0f;
+    public AudioClip doorOpenSound;
+    public AudioClip doorShutSound;
+
+    // Use this for initialization
+    void Start()
+    {
+        doorTimer = 0.0F;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doorIsOpen == true)
+        {
+            doorTimer += Time.deltaTime;
+            if (doorTimer > doorOpenTime)
+            {
+                Door(doorShutSound, false, "doorshut"); //door shut animation and sound
+                doorTimer = 0.0f;
+            }
+        }
+    }
+
+    void DoorCheck()
+    {
+        if(doorIsOpen == false) //if door is closed do...
+        {
+            Door(doorOpenSound, true, "dooropen");
+        }
+    }
+
+    void Door(AudioClip aClip, bool openCheck, string animName)
+    {
+        GetComponent<AudioSource>().PlayOneShot(aClip);
+        doorIsOpen = openCheck;
+        transform.parent.gameObject.GetComponent<Animation>().Play(animName);
+    }
+
+
+
+}
--- UGDE V2/Assets/Scripts/PlayerCollisions.cs
+++ UGDE V2/Assets/Scripts/PlayerCollisions.cs
@@ -4,34 +4,22 @@ using System.Collections;
 public class PlayerCollisions : MonoBehaviour {
 
     GameObject currentDoor;
-    private bool doorIsOpen = false;
-    private float doorTimer = 0.0f;
-
-    public float doorOpenTime = 3.0f;
-    public AudioClip doorOpenSound;
-    public AudioClip doorShutSound;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+    
 	// Update is called once per frame
 	void Update () {
-	    if(doorIsOpen == true)
+        RaycastHit hit;
+        
+        if(Physics.Raycast(transform.position, transform.forward, out hit, 3))
         {
-            doorTimer += Time.deltaTime;
-            if(doorTimer > doorOpenTime)
+            if(hit.collider.gameObject.tag == "playerDoor")
             {
-                Door(doorShutSound, false, "doorshut", currentDoor); //door shut animation and sound
-                doorTimer = 0.0f;
+                currentDoor = hit.collider.gameObject;
+                currentDoor.SendMessage("DoorCheck");
             }
         }
-
-
 	}
 
-    void OnControllerColliderHit(ControllerColliderHit hit)
+    /*void OnControllerColliderHit(ControllerColliderHit hit)
     {
         if(hit.gameObject.tag == "playerDoor" && doorIsOpen == false)
         {
@@ -40,7 +28,7 @@ public class PlayerCollisions : MonoBehaviour {
         }
     }
 
-    /*void OpenDoor(GameObject door)
+    void OpenDoor(GameObject door)
     {
         doorIsOpen = true;
         door.GetComponent<AudioSource>().PlayOneShot(doorOpenSound);
@@ -52,13 +40,13 @@ public class PlayerCollisions : MonoBehaviour {
         doorIsOpen = false;
         door.GetComponent<AudioSource>().PlayOneShot(doorShutSound);
         door.transform.parent.GetComponent<Animation>().Play("doorshut");
-    }*/
+    }
 
     void Door(AudioClip aClip, bool openCheck, string animName, GameObject thisDoor) // does both what functions OpenDoor() and ShutDoor() do in one function
     {
         thisDoor.GetComponent<AudioSource>().PlayOneShot(aClip);
         doorIsOpen = openCheck;
         thisDoor.transform.parent.GetComponent<Animation>().Play(animName);
-    }
+    }*/
 
 }
